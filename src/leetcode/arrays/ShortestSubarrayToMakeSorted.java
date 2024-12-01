6bc90947ac126106c53b79752fe5e0dba647601a
harshavardhan <haarshavardhan08@gmail.com>
2024-12-01 02:26:58 -0600

    Dec 01, 2024

---
+++ src/leetcode/arrays/ShortestSubarrayToMakeSorted.java
@@ -0,0 +1,34 @@
+package leetcode.arrays;
+
+public class ShortestSubarrayToMakeSorted {
+    public int findLengthOfShortestSubarray(int[] arr) {
+//        int shortest_window = 0;
+        int current_window = 0;
+        int left = 0, right = 1;
+        int prefix = 0;
+        int postfix = 0;
+
+        while (right < arr.length - 1){
+            if(arr[left] < arr[right]){
+                left++;
+                right++;
+                postfix = arr[right];
+            } else{
+                prefix = arr[left];
+                current_window = right - left;
+
+                right++;
+            }
+
+//            if(prefix <= postfix){
+//                shortest_window = Math.min(shortest_window, current_window);
+//            }
+        }
+        return current_window;
+    }
+
+    public static void main(String[] args) {
+        ShortestSubarrayToMakeSorted s = new ShortestSubarrayToMakeSorted();
+        System.out.println(s.findLengthOfShortestSubarray(new int[]{1,2,3,10,4,2,3,5}));
+    }
+}
